23e80c162835a8a1a749a67c1f13ee24d9a32f19
joaoguilherme09 <joaoguipmendes@gmail.com>
2026-06-13 17:18:06 -0300

    campeão id 27

--- data/data.js
+++ data/data.js
@@ -47,6 +47,7 @@ const DATA = {
     { id: 22, nome: "São Bernardo" },
     { id: 23, nome: "PSG" },
     { id: 24, nome: "PSV" },
+    { id: 25, nome: "Corinthians" },
   ],
 
   titulos: [
@@ -75,6 +76,8 @@ const DATA = {
     { id: 23, jogador_id: 10, campeonato_id: 1, time_id: 21, edicao: 5,  data: "2026-05-06" },
     { id: 24, jogador_id: 7,  campeonato_id: 6, time_id: 22, edicao: 2,  data: "2026-05-14" },
     { id: 25, jogador_id: 7,  campeonato_id: 3, time_id: 23, edicao: 6,  data: "2026-05-20" },
-    { id: 26, jogador_id: 6,  campeonato_id: 3, time_id: 24, edicao: 7,  data: "2026-06-02" }
+    { id: 26, jogador_id: 6,  campeonato_id: 3, time_id: 24, edicao: 7,  data: "2026-06-02" },
+    { id: 27, jogador_id: 9,  campeonato_id: 5, time_id: 25, edicao: 5,  data: "2026-06-13" }
   ]
 }
+
