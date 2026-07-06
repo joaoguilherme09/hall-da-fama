c64f33726b0c5a119d612dedc91351cba7b78c9b
joaoguilherme09 <joaoguipmendes@gmail.com>
2026-07-06 16:18:31 -0300

    edd 30

--- data/data.js
+++ data/data.js
@@ -52,7 +52,8 @@ const DATA = {
     { id: 24, nome: "PSV" },
     { id: 25, nome: "Corinthians" },
     { id: 26, nome: "Suiça" },
-    { id: 27, nome: "Guadalajara" }
+    { id: 27, nome: "Guadalajara" },
+    { id: 28, nome: "Celtic" }
   ],
 
   titulos: [
@@ -84,7 +85,8 @@ const DATA = {
     { id: 26, jogador_id: 6,  campeonato_id: 3, time_id: 24, edicao: 7,  data: "2026-06-02" },
     { id: 27, jogador_id: 9,  campeonato_id: 5, time_id: 25, edicao: 5,  data: "2026-06-13" },
     { id: 28, jogador_id: 11, campeonato_id: 1, time_id: 26, edicao: 6,  data: "2026-06-21" },
-    { id: 29, jogador_id: 12, campeonato_id: 2, time_id: 27, edicao: 6,  data: "2026-06-27" }
+    { id: 29, jogador_id: 12, campeonato_id: 2, time_id: 27, edicao: 6,  data: "2026-06-27" },
+    { id: 30, jogador_id: 7, campeonato_id: 3, time_id: 28, edicao: 8,  data: "2026-07-06" }
   ]
 }
 
